e0ff8bf47c5f8eb6da044897c6b53b3e1a636550
Dong Darong <dongdarong0713@gmail.com>
2026-01-12 14:24:58 +0700

    Modified code RolePermissionSeeder

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -2,18 +2,37 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RolePermissionSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        //
+        $adminPermissions = DB::table('permissions')->pluck('id');
+
+        foreach ($adminPermissions as $permissionId) {
+            DB::table('role_permissions')->insert([
+                'role_id' => 1,
+                'permission_id' => $permissionId
+            ]);
+        }
+
+        // Coach permissions (limited)
+        $coachPermissionNames = [
+            'player_view','player_update',
+            'team_view','match_view','match_update'
+        ];
+
+        $coachPermissions = DB::table('permissions')
+            ->whereIn('name', $coachPermissionNames)
+            ->pluck('id');
+
+        foreach ($coachPermissions as $permissionId) {
+            DB::table('role_permissions')->insert([
+                'role_id' => 2,
+                'permission_id' => $permissionId
+            ]);
+        }
     }
 }
