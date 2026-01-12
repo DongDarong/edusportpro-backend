96de1b559942b7aa4d35501b58e50f1e03bd9383
Dong Darong <dongdarong0713@gmail.com>
2026-01-12 14:25:46 +0700

    Modified code UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,18 +2,39 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            [
+                'role_id' => 1,
+                'name' => 'System Admin',
+                'email' => 'admin@edusportpro.com',
+                'password' => Hash::make('admin123456'),
+                'phone' => '012000001',
+                'status' => 'active'
+            ],
+            [
+                'role_id' => 2,
+                'name' => 'Coach Sok Dara',
+                'email' => 'coach@edusportpro.com',
+                'password' => Hash::make('coach123456'),
+                'phone' => '012000002',
+                'status' => 'active'
+            ],
+            [
+                'role_id' => 3,
+                'name' => 'Player Vannak',
+                'email' => 'player@edusportpro.com',
+                'password' => Hash::make('player123456'),
+                'phone' => '012000003',
+                'status' => 'active'
+            ],
+        ]);
     }
 }
